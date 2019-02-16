7fead3b7fc13a19da8973447a54895a67b672f0b
Максим <sdalll@mail.ru>
2019-02-17 01:09:26 +0300

    Task1

--- Population.cs
+++ Population.cs
@@ -6,15 +6,24 @@ namespace PadawansTask1
     {
         public static int GetYears(int initialPopulation, double percent, int visitors, int currentPopulation)
         {
+            if (initialPopulation <= 0 )
+            {
+                throw new ArgumentException();
+            }
+            if (currentPopulation <= 0)
+            {
+                throw new ArgumentException();
+            }
             // put your code here
             int Years = 0;
-           // throw new NotImplementedException();
-            while (initialPopulation <= currentPopulation)
-            {
-                initialPopulation = initialPopulation + (int)(initialPopulation * percent / 100) + visitors;
-                Years = Years + 1;
-            }
-            return Years;
-        }
+            // throw new NotImplementedException();
+                while (initialPopulation <= currentPopulation)
+                    {
+                        initialPopulation = initialPopulation + (int)(initialPopulation * percent / 100) + visitors;
+                        Years = Years + 1;
+                    }
+                    return Years;
+                
+    }
     }
 }
